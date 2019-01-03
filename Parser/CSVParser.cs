4cfb7653b4b9c5dd5af7818f1a1969340edfc60e
kotmw0701 <supurasyu@hotmail.co.jp>
2019-01-03 19:01:26 +0900

    メモの追加

--- Parser/CSVParser.cs
+++ Parser/CSVParser.cs
@@ -5,24 +5,37 @@ using System.Text;
 
 namespace Parser {
 	class CSVParser {
+		private string file = "example.txt";
 		private object[,] parsed;
 
+		public Encoding ParserEncoding { get; set; } = Encoding.UTF8;
 		public char Delimiter { get; set; } = ',';
 
-		public CSVParser(string file, Encoding encoding) {
-
+		public CSVParser(string file, Encoding encoding, char delimiter) {
+			this.file = file;
+			this.ParserEncoding = encoding;
+			this.Delimiter = delimiter;
 		}
 
-		public string[] getRow() {
+		public string[] ReadRows() {
 			return null;
 		}
 
-		public string[] getColumn() {
+		public string[] ReadColumns() {
 			return null;
 		}
 		
-		public string[,] getTable() {
+		public string[,] ReadTable() {
 			return null;
 		}
+
+		/* メモ
+		 *   実装のパターン
+		 *   ・file名指定の配列で返すパターン
+		 *   ・パーサークラス(ユーザー各位自作)に自動で入れちゃうパターン
+		 *   
+		 *   ・というかまずファイル名指定じゃなくてStreamReader入れられるようにして自動close(usingで囲むアレ)を使う側で使えるようにする
+		 * 
+		 */
 	}
 }
